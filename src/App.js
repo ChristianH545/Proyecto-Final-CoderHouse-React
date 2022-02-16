3a377c4e88f78887000bfa70dc1a144f10d26e63
ChristiaH545 <christianvenezuelahome@gmail.com>
2022-02-16 13:04:25 -0300

    colocando el nombre de nuestro sitio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import ItemListContainer from "./components/container/ItemListContainer";
 import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/container/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <>
-        <NavBar>S</NavBar>
+        <NavBar></NavBar>
+
         <ItemListContainer></ItemListContainer>
       </>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const NavBar = () => {
   return (
     <>
-      <h1>soy el NavBar</h1>
+      <NavContainer>
+        <h2>
+          Tuauto <span>Importado</span>
+        </h2>
+      </NavContainer>
     </>
   );
 };
 
 export default NavBar;
+
+const NavContainer = styled.nav;
