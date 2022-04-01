2592d88baf2dd2252671a6cb47d6f4f790e05526
ChristiaH545 <christianvenezuelahome@gmail.com>
2022-04-01 19:23:25 -0300

    haciendo un login con autentificacion firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react";
 import NavBar from "./layout/NavBar/NavBar";
 import HeroContainer from "./components/HeroContainer/HeroContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import React from "react";
+import Shop from "../src/pages/Shop";
+import Login from ".././src/components/ Login/Login";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [usuario, setusuario] = useState(null);
   return (
     <div className="App">
+      {usuario ? <Shop /> : <Login />}
       <NavBar />
       <Outlet />
       <HeroContainer />
---
+++ src/components/ Login/Login.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Uno from "../../assets/imagenes/img/img-auto1.jpg";
+import Dos from "../../assets/imagenes/img/img-auto2.jpg";
+import Tres from "../../assets/imagenes/img/img-auto3.jpg";
+import "../ Login/Login.css";
+
+const Login = () => {
+  const [registro, setRegistro] = useState(false);
+  return (
+    <>
+      <div className="row container p-4 ">
+        {/* esta parte sera donde se desarrollara el slide */}
+        <div className="col-md-6 mb-2 p-2">
+          <div
+            id="carouselExampleControls"
+            class="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={Uno} alt="" className="tamaño-img" />
+              </div>
+              <div className="carousel-item">
+                <img src={Dos} alt="" className="tamaño-img" />
+              </div>
+              <div class="carousel-item">
+                <img src={Tres} alt="" className="tamaño-img" />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+
+        {/* en esta seccion sera nuestro formulario */}
+        <div className="col-md-6 mt-5 p-5 ">
+          <div className="mt-5 ms-5">
+            <h1>{registro ? `Registrate` : `Iniciar Sesion`}</h1>
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Email Address: </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Ingresar Email"
+                  id="email"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">password: </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Ingresar password"
+                  id="password"
+                />
+              </div>
+              <button className="btn btn-primary" type="submit">
+                {registro ? `Registrate` : `Inicia Sesion`}
+              </button>
+            </form>
+
+            <div className="from-group">
+              <button
+                className="btn btn-secondary mt-4 form-control"
+                onClick={() => setRegistro(!registro)}
+              >
+                {registro
+                  ? `ya tienes una cuenta? Inicia Sesion`
+                  : `no tienes cuenta? Registrate`}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
